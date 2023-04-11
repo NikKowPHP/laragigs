900354944e88b1b1e956a58b243beccadb067ad3
NikKowPHP <nik.kow@outlook.com>
2023-04-11 19:59:36 +0200

    create a user controller, add methods to create and login user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
-    //
+    // Show register/create form
+    public function create()
+    {
+        return view('users.register');
+    }
+
+    // Create new user
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'confirmed', 'min:6' ],
+        ]);
+
+        // Hash 
+        $formFields['password'] = bcrypt($formFields['password']);
+
+
+        // create user
+        $user = User::create($formFields);
+
+        // login
+        auth()->login($user);
+        
+        return redirect('/')->with('message', 'User created and logged in');
+    }
 }
